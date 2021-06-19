392430cf23cdccc9beb78cc7544fabeee28e6aa8
jiikko <n905i.1214@gmail.com>
2021-06-19 07:40:15 +0100

    Update core

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', tag: "0.1"
+gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', tag: "0.1.1"
--- app.rb
+++ app.rb
@@ -1,21 +1,33 @@
 #!/usr/bin/env ruby
-require 'bundler/setup'
+
 require 'procon_bypass_man'
+require 'procon_bypass_man-splatoon2'
+
+ProconBypassMan.tap do |pbm|
+  pbm.logger = "./app.log"
+  pbm.logger.level = :debug
+end
+
+fast_return = ProconBypassMan::Splatoon2::Macro::FastReturn
+guruguru = ProconBypassMan::Splatoon2::Mode::Guruguru
 
 ProconBypassMan.run do
+  install_macro_plugin fast_return
+  install_mode_plugin guruguru
+
   prefix_keys_for_changing_layer [:zr, :r, :zl, :l]
 
   layer :up, mode: :manual do
-    flip :zr, if_pushed: :zr, force_neutral: :zl
-    flip :zl, if_pushed: [:y, :b, :zl]
-    flip :down, if_pushed: :down
-    macro :fast_return, if_pushed: [:y, :b, :down]
+    flip :zr, if_pressed: :zr, force_neutral: :zl
+    flip :zl, if_pressed: [:y, :b, :zl]
+    flip :down, if_pressed: :down
+    macro fast_return.name, if_pressed: [:y, :b, :down]
   end
-  layer :right, mode: :guruguru
+  layer :right, mode: guruguru.name
   layer :left do
-    # no-op
   end
   layer :down do
     flip :zl
   end
 end
+
