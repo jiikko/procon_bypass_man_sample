f3822e88d3216ff2418e20851061120a657f2b0d
jiikko <n905i.1214@gmail.com>
2021-06-25 13:02:59 +0100

    PBMの0.1.2に対応する

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # procon_bypass_man_sample
-https://github.com/splaspla-hacker/procon_bypass_man
+https://github.com/splaspla-hacker/procon_bypass_man を使ったサンプルです
 
 ## インストール
+###
 ```
-sudo bundle install
+git clone https://github.com/jiikko/procon_bypass_man_sample.git
+cd procon_bypass_man_sample
+rbenv install 3.0.0
+sudo gem i bundler
+sudo bundle install --path vendor/bundle
 ```
 
 ## 使い方
-
 ```shell
-sudo ruby app.rb
+bundle exec sudo ruby app.rb
 ```
--- app.rb
+++ app.rb
@@ -9,26 +9,4 @@ ProconBypassMan.tap do |pbm|
   pbm.logger.level = :debug
 end
 
-fast_return = ProconBypassMan::Splatoon2::Macro::FastReturn
-guruguru = ProconBypassMan::Splatoon2::Mode::Guruguru
-
-ProconBypassMan.run do
-  install_macro_plugin fast_return
-  install_mode_plugin guruguru
-
-  prefix_keys_for_changing_layer [:zr, :r, :zl, :l]
-
-  layer :up, mode: :manual do
-    flip :zr, if_pressed: :zr, force_neutral: :zl
-    flip :zl, if_pressed: [:y, :b, :zl]
-    flip :down, if_pressed: :down
-    macro fast_return.name, if_pressed: [:y, :b, :down]
-  end
-  layer :right, mode: guruguru.name
-  layer :left do
-  end
-  layer :down do
-    flip :zl
-  end
-end
-
+ProconBypassMan.run(setting_path: "./setting.yml")
