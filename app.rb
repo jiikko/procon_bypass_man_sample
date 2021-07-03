ff50b23015c10143bbd794369a726cd9d18f65a6
jiikko <n905i.1214@gmail.com>
2021-07-03 18:51:54 +0100

    はい

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require 'procon_bypass_man/splatoon2'
 ProconBypassMan.tap do |pbm|
   pbm.logger = Logger.new("#{ProconBypassMan.root}/app.log", 5, 1024 * 1024 * 10)
   pbm.logger.level = :debug
+  pbm.root = File.expand_path(__dir__)
 end
 
 ProconBypassMan.run(setting_path: "./setting.yml")
