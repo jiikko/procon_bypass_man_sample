83e237ba34917db8df6ab1938f9f9c17e1f9b40a
jiikko <n905i.1214@gmail.com>
2021-07-18 22:24:48 +0900

    bundlerのキャッシュでエラーになったら消すようにする

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@
 
 require 'bundler/inline'
 
-gemfile do
-  source 'https://rubygems.org'
-  git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-  gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', branch: "edge"
-  gem 'procon_bypass_man-splatoon2', github: 'splaspla-hacker/procon_bypass_man-splatoon2', branch: "0.1.0"
+ProconBypassMan.remove_bundler_cache_if_need do
+  gemfile do
+    source 'https://rubygems.org'
+    git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+    gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', branch: "edge"
+    gem 'procon_bypass_man-splatoon2', github: 'splaspla-hacker/procon_bypass_man-splatoon2', branch: "0.1.0"
+  end
 end
 
 ProconBypassMan.tap do |pbm|
--- web.rb
+++ web.rb
@@ -2,11 +2,13 @@
 
 require 'bundler/inline'
 
-gemfile do
-  source 'https://rubygems.org'
-  git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-  gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', branch: "edge"
-  gem 'procon_bypass_man-web', github: 'splaspla-hacker/procon_bypass_man-web'
+ProconBypassMan.remove_bundler_cache_if_need do
+  gemfile do
+    source 'https://rubygems.org'
+    git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+    gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', branch: "edge"
+    gem 'procon_bypass_man-web', github: 'splaspla-hacker/procon_bypass_man-web'
+  end
 end
 
 ProconBypassMan::Web::Server.start
