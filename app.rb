9d960576ed64972893fa59c69d2b815414f012bb
jiikko <n905i.1214@gmail.com>
2021-07-18 22:55:12 +0900

    NameErrorになったのをなおす

--- app.rb
+++ app.rb
@@ -1,13 +1,28 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
 require 'bundler/inline'
 
-ProconBypassMan.remove_bundler_cache_if_need do
+def is_correct_directory_to_remove?(dir)
+  !!(%r!^/home/pi/.rbenv/versions/! =~ dir)
+end
+
+begin
   gemfile do
     source 'https://rubygems.org'
     git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
     gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', branch: "edge"
     gem 'procon_bypass_man-splatoon2', github: 'splaspla-hacker/procon_bypass_man-splatoon2', branch: "0.1.0"
+  end
+  # TODO  bundlerのバージョンを指定する
+rescue Bundler::Source::Git::GitCommandError => e
+  if %r!If this error persists you could try removing the cache directory '([^']+)'! =~ e.to_s
+    if is_correct_directory_to_remove?($1)
+      FileUtiles.rm_rf($1)
+      puts "Bundler::Source::Git::GitCommandErrorが起きたので問題のディレクトリを削除しました。"
+    else
+      raise "bundlerのキャッシュディレクトリを削除できませんでした"
+    end
   end
 end
 
--- web.rb
+++ web.rb
@@ -1,13 +1,27 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
 require 'bundler/inline'
 
-ProconBypassMan.remove_bundler_cache_if_need do
+def is_correct_directory_to_remove?(dir)
+  !!(%r!^/home/pi/.rbenv/versions/! =~ dir)
+end
+
+begin
   gemfile do
     source 'https://rubygems.org'
     git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
     gem 'procon_bypass_man', github: 'splaspla-hacker/procon_bypass_man', branch: "edge"
     gem 'procon_bypass_man-web', github: 'splaspla-hacker/procon_bypass_man-web'
+  end
+rescue Bundler::Source::Git::GitCommandError => e
+  if %r!If this error persists you could try removing the cache directory '([^']+)'! =~ e.to_s
+    if is_correct_directory_to_remove?($1)
+      FileUtiles.rm_rf($1)
+      puts "Bundler::Source::Git::GitCommandErrorが起きたので問題のディレクトリを削除しました。"
+    else
+      raise "bundlerのキャッシュディレクトリを削除できませんでした"
+    end
   end
 end
 
