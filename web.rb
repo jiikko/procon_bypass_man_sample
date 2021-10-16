cb158258f3673a706ee8789c38df02319e188e6b
jiikko <n905i.1214@gmail.com>
2021-10-16 16:51:14 +0900

    use stable

--- web.rb
+++ web.rb
@@ -2,27 +2,10 @@
 
 require 'bundler/inline'
 
-def is_correct_directory_to_remove?(dir)
-  !!(%r!^/home/pi/.rbenv/versions/! =~ dir)
-end
-
-begin
-  gemfile do
-    source 'https://rubygems.org'
-    git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-    gem 'procon_bypass_man-web', github: 'splaplapla/procon_bypass_man-web', branch: "edge"
-  end
-rescue Bundler::Source::Git::GitCommandError => e
-  require 'fileutils'
-  if %r!If this error persists you could try removing the cache directory '([^']+)'! =~ e.to_s
-    if is_correct_directory_to_remove?($1)
-      FileUtils.rm_rf($1)
-      puts "Bundler::Source::Git::GitCommandErrorが起きたので問題のディレクトリを削除しました。"
-      exit 1
-    else
-      raise "bundlerのキャッシュディレクトリを削除できませんでした"
-    end
-  end
+gemfile do
+  source 'https://rubygems.org'
+  git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+  gem 'procon_bypass_man-web', '0.1.1'
 end
 
 ProconBypassMan::Web::Server.start
