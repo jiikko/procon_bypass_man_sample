0aec53cabdb0862b17ce920b23e0036f3bb1deae
jiikko <n905i.1214@gmail.com>
2022-01-22 18:07:37 +0900

    使っていないメソッドを削除する

--- web.rb
+++ web.rb
@@ -2,10 +2,6 @@
 
 require 'bundler/inline'
 
-def is_correct_directory_to_remove?(dir)
-  !!(%r!^/home/pi/.rbenv/versions/! =~ dir)
-end
-
 gemfile do
   source 'https://rubygems.org'
   git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
